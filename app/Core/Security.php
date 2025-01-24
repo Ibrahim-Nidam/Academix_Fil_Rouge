5b2cd7dcc9d39b1a75423eddbe4163097207a4cc
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 14:48:37 +0100

    CLASS: updated class security

--- app/Core/Security.php
+++ app/Core/Security.php
@@ -5,5 +5,12 @@ namespace Core;
 
 class Security {
 
-    
+    private $passwordPattern = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/";
+    private $csrfTokenKey = "csrf_token";
+
+    public function __construct() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
 }
