a895374d056340457a757d891c773406a23063ed
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 11:22:19 +0100

    CLASS: added the methodes to show view and redirect to page in the controller class

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -4,5 +4,39 @@ namespace Core;
 
 
 class Controller {
+    protected $security;
+    protected $router;
+
+    public function __construct(){
+        $this->security = new Security;
+        $this->router = new Router();
+    }
+
+    protected function showView($view, $data = []){
+        extract($data);
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $flashMessages = [];
+        if (isset($_SESSION['flash'])) {
+            $flashMessages = $_SESSION['flash'];
+            unset($_SESSION['flash']);
+        }
+
+        $viewPath = __DIR__ . "/../../app/Views/{$view}.php";
+        if (file_exists($viewPath)) {
+            require_once $viewPath;
+        } else {
+            die("View '{$view}' not found");
+        }
+    }
+
+    protected function redirect($url){
+        header("Location: {$url}");
+        exit();
+    }
+
     
 }
