eff49b43bfd58a8de7def5a0781c4d8407220638
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 15:33:49 +0100

    CLASS: csrf middleware update

--- app/Middleware/CsrfMiddleware.php
+++ app/Middleware/CsrfMiddleware.php
@@ -2,6 +2,18 @@
 
 namespace Middleware;
 
-class CsrfMiddleware {
+use Core\Controller;
+use Core\Router;
+use Core\Security;
 
+class CsrfMiddleware {
+    private $security;
+    private $router;
+    private $controller;
+
+    public function __construct(){
+        $this->security = new Security();
+        $this->router = new Router();
+        $this->controller = new Controller();
+    }
 }
