7d826f793b33a29fcaeabe1a94f8e61d32e1b8d0
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 11:33:07 +0100

    CLASS: added methodes for future soft notifications

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -38,5 +38,22 @@ class Controller {
         exit();
     }
 
-    
+    protected function setFlash($key, $message){
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $_SESSION['flash'][$key] = $message;
+    }
+
+    protected function getFlash($key){
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (isset($_SESSION['flash'][$key])) {
+            $message = $_SESSION['flash'][$key];
+            unset($_SESSION['flash'][$key]);
+            return $message;
+        }
+        return null;
+    }
 }
