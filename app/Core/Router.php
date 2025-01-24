03b418fbbf1971c13c145c4c21da593cffd0d8d6
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 13:11:39 +0100

    CLASS: route method handler added

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -12,4 +12,28 @@ class Router {
     public function post($uri, $controller){
         $this->routes["POST"][$uri] = $controller;
     }
+
+    public function route($uri, $method) {
+        $routes = $this->routes[$method] ?? [];
+    
+        foreach ($routes as $route => $controller) {
+            $pattern = preg_replace('/\{[a-zA-Z0-9_]+\}/', '([0-9]+)', $route);
+            if (preg_match("#^$pattern$#", $uri, $matches)) {
+                array_shift($matches);
+    
+                if (is_array($controller)) {
+                    [$class, $method] = $controller;
+    
+                    if (!class_exists($class)) {
+                        die("Class not found: $class");
+                    }
+    
+                    $controller = new $class();
+                    return call_user_func_array([$controller, $method], $matches);
+                }
+            }
+        }
+    
+        $this->handleError(404);
+    }
 }
