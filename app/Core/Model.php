ad3238031b2c1b9c7a9be839121dc508016ed3bc
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 12:49:05 +0100

    CLASS: linked the main model to the database

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -2,7 +2,39 @@
 
 namespace Core;
 
+use PDO;
+use PDOException;
 
 class Model {
-    
+    protected $db;
+        protected $security;
+
+        public function __construct() {
+            $this->security = new Security();
+            $config = require __DIR__ . "/../../Config/database.php";
+
+            try {
+                $dsn = "mysql:host={$config['host']};port={$config['port']}";
+                $this->db = new PDO($dsn, $config["username"], $config["password"]);
+                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $stmt = $this->db->prepare("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = :dbname");
+                $stmt->execute([":dbname" => $config["database"]]);
+                $dbExists = $stmt->fetch();
+
+                if(!$dbExists){
+                    $this->db->exec("CREATE DATABASE `{$config['database']}` CHARACTER SET {$config['charset']}");
+                    $this->db->exec("USE `{$config['database']}`").
+                    $this->initializeDatabase($config["script_path"]);
+
+                    $seeder = new DatabaseSeeder();
+                    $seeder->run();
+                } else {
+                    $this->db->exec("USE `{$config['database']}`");
+                }
+            } catch (PDOException $e){
+                error_log("Database connection error : " . $e->getMessage());
+                die("An error occurred. Please try again later.");
+            }
+        }
 }
