9642191053f630b2d9713fd9a4b5aee6c03e06f3
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 11:19:22 +0100

    CLASS: added the methodes and properties used

--- app/Core/App.php
+++ app/Core/App.php
@@ -3,5 +3,26 @@
     namespace Core;
 
     class App {
-        
+        public static $router;
+
+        public function __construct(){
+            self::$router = new Router();
+        }
+
+        public function run() {
+            $uri = rawurldecode(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+    
+            $scriptName = dirname($_SERVER["SCRIPT_NAME"]);
+            $basePath = rtrim($scriptName, '/');
+    
+            if (strpos($uri, $basePath) === 0) {
+                $uri = substr($uri, strlen($basePath));
+            }
+    
+            $uri = '/' . ltrim($uri, '/');
+    
+            $method = $_SERVER["REQUEST_METHOD"];
+    
+            self::$router->route($uri, $method);
+        }
     }
