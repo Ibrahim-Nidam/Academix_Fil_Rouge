e8baf56a0205474f06dacd5c39aeb0da7a1eaebe
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 15:35:31 +0100

    CLASS: csrf middleware handle request method added

--- app/Middleware/CsrfMiddleware.php
+++ app/Middleware/CsrfMiddleware.php
@@ -16,4 +16,17 @@ class CsrfMiddleware {
         $this->router = new Router();
         $this->controller = new Controller();
     }
+
+    public function handle($request){
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            $csrfToken = $request["csrf_token"] ?? null;
+
+            if(!$csrfToken || !$this->security->verifyCsrfToken($csrfToken)){
+                http_response_code(403);
+                $this->router->handleError(403);
+                $this->controller->setFlash("error","Forbidden: Invalid CSRF Token.");
+            }
+        }
+        return true;
+    }
 }
