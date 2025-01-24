bf5fdca6545a25908691aa26254e8c6e1e6e5786
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 11:39:07 +0100

    CLASS: added some methodes in seeder class for future use

--- app/Core/DatabaseSeeder.php
+++ app/Core/DatabaseSeeder.php
@@ -6,5 +6,15 @@ use Faker\Factory;
 
 class DatabaseSeeder extends Model
 {
-   
+    private $faker;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->faker = Factory::create();
+    }
+
+    public function run(){
+
+    }
 }
