083add86168d34ad360287e0ab0ca8892c3ff977
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 12:59:41 +0100

    CLASS: generic single insert methode added

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -67,4 +67,15 @@ class Model {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function createSingle(string $table, array $data): int {
+        $columns = implode(",", array_keys($data));
+        $placeholders = ":" . implode(", :", array_keys($data));
+        $sql = "INSERT IGNORE INTO {$table} ({$columns}) VALUES ({$placeholders})";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($data);
+
+        return $this->db->lastInsertId();
+    }
+
 }
