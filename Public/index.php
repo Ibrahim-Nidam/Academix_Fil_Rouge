55a7951abf1b6c314a4599b6d67724c3c331912c
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-24 15:58:21 +0100

    CONFIG: linked the dotenv to the index

--- Public/index.php
+++ Public/index.php
@@ -4,6 +4,9 @@ use Core\App;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 $app = new App();
 
 require_once __DIR__ . '/../config/routes.php';
