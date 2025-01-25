34cdda2f4d865fb59805e36b35a363e4c47d8fd6
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-01-25 22:46:22 +0100

    LICENCE : added some copyright material in my app.php file

--- app/Core/App.php
+++ app/Core/App.php
@@ -1,28 +1,33 @@
 <?php
+/*
+ * Copyright © 2024 Ibrahim Nidam
+ * All rights reserved.
+ * Unauthorized use of this file, via any medium, is strictly prohibited.
+ */
 
-    namespace Core;
+namespace Core;
 
-    class App {
-        public static $router;
+class App {
+    public static $router;
 
-        public function __construct(){
-            self::$router = new Router();
-        }
-
-        public function run() {
-            $uri = rawurldecode(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
-    
-            $scriptName = dirname($_SERVER["SCRIPT_NAME"]);
-            $basePath = rtrim($scriptName, '/');
-    
-            if (strpos($uri, $basePath) === 0) {
-                $uri = substr($uri, strlen($basePath));
-            }
-    
-            $uri = '/' . ltrim($uri, '/');
-    
-            $method = $_SERVER["REQUEST_METHOD"];
-    
-            self::$router->route($uri, $method);
-        }
+    public function __construct(){
+        self::$router = new Router();
     }
+
+    public function run() {
+        $uri = rawurldecode(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+
+        $scriptName = dirname($_SERVER["SCRIPT_NAME"]);
+        $basePath = rtrim($scriptName, '/');
+
+        if (strpos($uri, $basePath) === 0) {
+            $uri = substr($uri, strlen($basePath));
+        }
+
+        $uri = '/' . ltrim($uri, '/');
+
+        $method = $_SERVER["REQUEST_METHOD"];
+
+        self::$router->route($uri, $method);
+    }
+}
