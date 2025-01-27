25871f42eb088fa1a27267a920beb95849cfc661
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-01-27 20:56:33 +0100

    CLASS: absract middleware added with a method for polymorphisme

---
+++ app/Core/Middleware.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copyright © 2024 Ibrahim Nidam
+ * All rights reserved.
+ * Unauthorized use of this file, via any medium, is strictly prohibited.
+ */
+namespace Core;
+
+abstract class Middleware {
+    abstract public function handle();
+}
