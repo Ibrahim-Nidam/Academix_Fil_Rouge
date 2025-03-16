6c82dd03dcc78fcdbf079132674d920440d44103
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 14:15:30 +0000

    STYLE: partial file updated with customized styles from the color palete of the project

--- resources/views/partials/admin/styles.blade.php
+++ resources/views/partials/admin/styles.blade.php
@@ -10,3 +10,35 @@
 <!-- FullCalendar JS -->
 <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+
+<script>
+  tailwind.config = {
+  darkMode: 'class',
+  theme: {
+      extend: {
+          colors: {
+              primary: {
+                  light: '#f3f4f6',
+                  dark: '#0d1321',
+                  text: {
+                      light: '#1f2937',
+                      dark: '#f3f4f6'
+                  },
+                  accent: '#d4af37',
+                  blue: '#4260a6',
+                  yellow: '#e5cf86',
+                  present: '#7a92ca',
+                  absent: '#d4af37',
+                  late: '#bdc9e5',
+                  performance: {
+                      high: '#4260a6',
+                      medium: '#5c7aa4',
+                      low: '#d4af37'
+                    }
+              }
+          }
+      }
+  }
+  }
+  </script>
+  
