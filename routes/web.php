8ee0f65d7e78c08dd0b627b7938180ef201354fd
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 22:22:07 +0000

    ROUTE: admin route updated with auth middleware and admin role param

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
-Route::view('/importData', 'admin.importData')->name('admin.importData');
-Route::view('/planning', 'admin.planningPage')->name('admin.planningPage');
-Route::view('/users', 'admin.usersPage')->name('admin.usersPage');
+
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
+    Route::view('/importData', 'admin.importData')->name('admin.importData');
+    Route::view('/planning', 'admin.planningPage')->name('admin.planningPage');
+    Route::view('/users', 'admin.usersPage')->name('admin.usersPage');
+});
