3ee8a872c261ded7a800f8c4fbd4a7fb11081842
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 15:27:13 +0000

    VIEW: import data updated

--- resources/views/admin/importData.blade.php
+++ resources/views/admin/importData.blade.php
@@ -59,7 +59,7 @@
       </div>
     </div>
 
-
+    {{-- Import Button --}}
     <div class="flex flex-col md:flex-row gap-3 justify-end">
       <button id="cancel-import" class="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors duration-300 text-sm md:text-base">
         Cancel
