39f6c4e1a9cd2c37d8bb3c17cffda01d79216915
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 22:06:17 +0000

    ROUTE: admin users management added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
 Route::view('/importData', 'admin.importData')->name('admin.importData');
 Route::view('/planning', 'admin.planningPage')->name('admin.planningPage');
+Route::view('/users', 'admin.usersPage')->name('admin.usersPage');
