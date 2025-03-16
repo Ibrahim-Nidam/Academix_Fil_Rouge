1f1b436177dd7a0f05b2b32166297ed4d326ecb7
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 14:13:52 +0000

    STYLE: partial file updated with cdn scripts

---
+++ resources/views/partials/admin/styles.blade.php
@@ -0,0 +1,12 @@
+<!-- TailwindCSS -->
+<script src="https://cdn.tailwindcss.com"></script>
+
+<!-- Chart.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
+
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+<!-- FullCalendar JS -->
+<link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
