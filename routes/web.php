7c59cb02555e8f4bcc4369a5204e6f1e74224233
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 15:14:52 +0000

    ROUTE: admin import data added

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
+Route::view('/importData', 'admin.importData')->name('admin.importData');
