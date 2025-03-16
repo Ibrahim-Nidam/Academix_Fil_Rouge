aa19ab6eece0df72312c95454e6b2d460351f06e
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 15:33:42 +0000

    ROUTE: admin planning added

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
 Route::view('/importData', 'admin.importData')->name('admin.importData');
+Route::view('/planning', 'admin.planningPage')->name('admin.planningPage');
