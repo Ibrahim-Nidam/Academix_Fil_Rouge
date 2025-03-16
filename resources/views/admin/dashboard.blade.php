3904e5feb2c944193b2d4c19cd68569d4ea4421f
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-16 15:02:23 +0000

    VIEW: admin dash updated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
   <div class="flex flex-col flex-1 p-4 md:p-6 w-full">
+
+{{-- WELCOME SECTION --}}
     <div class="bg-white dark:bg-gray-800 rounded-xl shadow-md p-4 md:p-6 mb-4 md:mb-6">
       <h1 class="text-2xl md:text-3xl font-bold">Welcome back, <span class="text-primary-accent">Dr. Smith</span></h1>
       <p class="text-gray-600 dark:text-gray-400 mt-2 text-sm md:text-base">
@@ -11,5 +13,9 @@
         <span class="italic">Here's an overview of today's progress</span>
       </p>
     </div>
+
+    
+
+
   </div>
 @endsection
