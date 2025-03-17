f93ab1b9f0c805e053686e0f678220185a2106b9
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 10:05:12 +0000

    JAVASCRIPT: dashboard script file updated with header date

---
+++ resources/views/partials/admin/js/dashScript.blade.php
@@ -0,0 +1,7 @@
+<script>
+    // header date
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById('current-date').textContent = now.toLocaleDateString('en-US', options);
+
+</script>
