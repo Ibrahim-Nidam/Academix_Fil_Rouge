d4300df8b1357dcf29efb09c245a81a97a062a44
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 22:23:55 +0000

    JAVASCRIPT: teacher dashboard script file updated with performance buttons toggle

--- resources/views/partials/teacher/js/dashScript.blade.php
+++ resources/views/partials/teacher/js/dashScript.blade.php
@@ -10,5 +10,25 @@ const lowestPerformersBtn = document.getElementById('lowestPerformersBtn');
 const topPerformers = document.getElementById('topPerformers');
 const lowestPerformers = document.getElementById('lowestPerformers');
 
+topPerformersBtn.addEventListener('click', () => {
+    topPerformersBtn.classList.add('toggle-btn-active');
+    topPerformersBtn.classList.remove('toggle-btn-inactive');
+    lowestPerformersBtn.classList.add('toggle-btn-inactive');
+    lowestPerformersBtn.classList.remove('toggle-btn-active');
+
+    lowestPerformers.classList.add('hidden');
+    topPerformers.classList.remove('hidden');
+});
+
+lowestPerformersBtn.addEventListener('click', () => {
+    lowestPerformersBtn.classList.add('toggle-btn-active');
+    lowestPerformersBtn.classList.remove('toggle-btn-inactive');
+    topPerformersBtn.classList.add('toggle-btn-inactive');
+    topPerformersBtn.classList.remove('toggle-btn-active');
+
+    topPerformers.classList.add('hidden');
+    lowestPerformers.classList.remove('hidden');
+});
+
 
 </script>
