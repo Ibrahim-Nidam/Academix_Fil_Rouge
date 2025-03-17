033162b6ba5952661caaa2d371bd10527bb55520
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 13:53:04 +0000

    VIEW: global profile security page updated with page header and using admin layout

---
+++ resources/views/global/profile_settings/security.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('title', 'Profile Settings')
+
+@include('global.profile_settings.styles')
+
+@section('content')
+{{-- header page --}}
+    <div class="flex-1 p-5 px-16">
+        <div class="mb-10 flex items-center gap-6 flex-wrap">
+            <h1 class="text-4xl font-bold text-primary-blue dark:text-primary-text-dark">Good Morning</h1>
+            <h2 class="text-2xl text-gray-500 dark:text-gray-400 mt-1">Mr. Johnson Maduka</h2>
+        </div>
+        
+        
+    </div>
+@endsection
