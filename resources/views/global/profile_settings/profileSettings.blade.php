f02cf15ce7281489987b5eaaad61906fe6c88b58
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 13:48:01 +0000

    VIEW: global profile settings page updated with page switch buttons

--- resources/views/global/profile_settings/profileSettings.blade.php
+++ resources/views/global/profile_settings/profileSettings.blade.php
@@ -11,6 +11,16 @@
             <h1 class="text-4xl font-bold text-primary-blue dark:text-primary-text-dark">Good Morning</h1>
             <h2 class="text-2xl text-gray-500 dark:text-gray-400 mt-1">Mr. Johnson Maduka</h2>
         </div>
-        
+        {{-- security & info buttons --}}
+        <div class="md:ml-20">
+            <div class="border-b border-gray-200 dark:border-gray-700 mb-10">
+                <div class="flex space-x-10">
+                    <a href="{{route('global.profile_settings.profileSettings')}}" class="tab-active py-4 px-1 font-medium dark:text-primary-text-dark">Edit Profile</a>
+                    <a href="{{route('global.profile_settings.security')}}" class="tab-inactive py-4 px-1 font-medium dark:text-primary-text-dark">Security</a>
+                </div>
+            </div>
+            
+            
+        </div>
     </div>
 @endsection
