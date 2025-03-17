e6541bc88dfde0bc818f84174f95113367cb8956
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 13:59:13 +0000

    VIEW: global profile security page updated with page switch buttons

--- resources/views/global/profile_settings/security.blade.php
+++ resources/views/global/profile_settings/security.blade.php
@@ -12,6 +12,16 @@
             <h2 class="text-2xl text-gray-500 dark:text-gray-400 mt-1">Mr. Johnson Maduka</h2>
         </div>
         
-        
+        <div class="md:ml-20">
+            {{-- security & info buttons --}}
+            <div class="border-b border-gray-200 dark:border-gray-700 mb-10">
+                <div class="flex space-x-10">
+                    <a href="{{route('global.profile_settings.profileSettings')}}" class="tab-inactive py-4 px-1 font-medium dark:text-primary-text-dark">Edit Profile</a>
+                    <a href="{{route('global.profile_settings.security')}}" class="tab-active py-4 px-1 font-medium dark:text-primary-text-dark">Security</a>
+                </div>
+            </div>
+            
+            
+        </div>
     </div>
 @endsection
