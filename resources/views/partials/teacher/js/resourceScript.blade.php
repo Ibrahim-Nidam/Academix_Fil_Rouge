7e28a92eda99afb9c7b745a96de6385c099976e9
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 22:48:16 +0000

    JAVASCRIPT: teacher resource script file updated with page variables

---
+++ resources/views/partials/teacher/js/resourceScript.blade.php
@@ -0,0 +1,12 @@
+<script>
+  const uploadBtn = document.getElementById('uploadBtn');
+  const uploadModal = document.getElementById('uploadModal');
+  const modalOverlay = document.getElementById('modalOverlay');
+  const closeModal = document.getElementById('closeModal');
+  const cancelUploadBtn = document.getElementById('cancelUploadBtn');
+  const uploadArea = document.getElementById('uploadArea');
+  const browseBtn = document.getElementById('browseBtn');
+  const fileInput = document.getElementById('fileInput');
+  
+  
+</script>
