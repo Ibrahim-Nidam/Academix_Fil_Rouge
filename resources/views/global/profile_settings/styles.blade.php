85e329fd014327b3553cdf12260de0c251f8e2b5
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-17 14:01:55 +0000

    STYLE: profile settings partial updated

---
+++ resources/views/global/profile_settings/styles.blade.php
@@ -0,0 +1,19 @@
+<style type="text/tailwindcss">
+    .tab-active {
+        color: #4260a6;
+        border-bottom: 3px solid #4260a6;
+    }
+
+    .tab-inactive {
+        color: #9ca3af;
+        border-bottom: 3px solid transparent;
+    }
+    .dark .tab-active {
+        color: #f3f4f6;
+        border-bottom: 3px solid #d4af37;
+    }
+    .dark .tab-inactive {
+        color: #9ca3af;
+        border-bottom: 3px solid transparent;
+    }
+</style>
