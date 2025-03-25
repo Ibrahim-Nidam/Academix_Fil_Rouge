89c087771823c9f0e01a77cd92831ac928b6dc31
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-25 13:29:10 +0000

    MODEL: user BUG fix where duplicates crash the application

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -71,9 +73,8 @@ class User extends Authenticatable
     {
         $cleanFirstName = preg_replace('/[^a-zA-Z0-9]/', '', $firstname);
         $cleanLastName = preg_replace('/[^a-zA-Z0-9]/', '', $lastname);
-
         $username = strtolower($cleanFirstName) . ucfirst(strtolower($cleanLastName));
-
+        
         return $username;
     }
 
@@ -84,22 +85,42 @@ class User extends Authenticatable
         return strtolower($cleanFirstName . '.' . $cleanLastName) . '@school.com';
     }
 
-    public static function createBulkUsers($usersData, $role)
+    public static function createBulkUsers($usersData, $roleType)
     {
         $createdUsers = [];
+        $errors = [];
 
-        foreach($usersData as $userData) {
-            if(empty($userData['username'])){
-                $userData['username'] = self::generateUsername($userData['first_name'], $userData['last_name']);
+        foreach ($usersData as $userData) {
+            try {
+                if (empty($userData['username'])) {
+                    $userData['username'] = self::generateUsername(
+                        $userData['first_name'],
+                        $userData['last_name']
+                    );
+                }
+
+                $userData['email'] = self::generateEmail(
+                    $userData['first_name'],
+                    $userData['last_name']
+                );
+
+                $userData['password'] = bcrypt($userData['username']);
+                $userData['role'] = $roleType;
+
+                $user = self::create($userData);
+                $createdUsers[] = $user;
+
+            } catch (Exception $e) {
+                $errors[] = [
+                    'user' => $userData,
+                    'error' => $e->getMessage()
+                ];
             }
-
-            $userData['email'] = self::generateEmail($userData['first_name'], $userData['last_name']);
-
-            $userData['password'] = bcrypt($userData['username']);
-            $userData['role'] = $role;
-
-            $createdUsers[] = self::create($userData);
         }
-        return $createdUsers;
+
+        return [
+            'created' => $createdUsers,
+            'errors' => $errors
+        ];
     }
 }
