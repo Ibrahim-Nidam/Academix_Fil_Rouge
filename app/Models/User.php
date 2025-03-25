d7b20f2e5c7ae4fe971f23d64553647fd209194e
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-25 11:01:14 +0000

    MODEL: user updated with uuid logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +22,15 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'id',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'gender',
+        'role',
+        'created_at',
+        'updated_at'
     ];
 
     /**
@@ -44,5 +54,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function($model){
+            if(empty($model->id)){
+                $model->id = (string) Str::orderedUuid();
+            }
+        });
     }
 }
