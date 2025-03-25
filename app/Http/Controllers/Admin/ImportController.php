0cdd69d3e24b0e62ab7308aad7a4fae3b242d2eb
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-25 13:55:33 +0000

    CONTROLLER: import updated with a helper function to check the header of the excel file

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
-
 class ImportController extends Controller
 {
-    
+    private function hasHeaderRow(array $row)
+    {
+        foreach ($row as $cell){
+            if(!empty($cell) && preg_match('/(first|last|name|gender)/i', $cell)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
