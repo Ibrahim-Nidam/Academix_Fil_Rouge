0d7633312f6e0cfe81aa7be5772d7d5c532e4468
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-26 11:18:43 +0000

    CONTROLLER: session updated with login method

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
-    
+
+    public function login(){
+        $validation = request()->validate([
+            'login' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credantials = filter_var($validation['login'], FILTER_VALIDATE_EMAIL) 
+                        ? ['email' => $validation['login'], 'password' => $validation['password']]
+                        : ['username' => $validation['login'], 'password' => $validation['password']];
+
+                        // dd($credantials);
+        if(!Auth::attempt($credantials)){
+            throw ValidationException::withMessages([
+                'login' => 'The provided credentials do not match'
+            ]);
+        }
+
+        request()->session()->regenerate();
+        $role = Auth::user()->role;
+
+        return redirect($role . '/dashboard');
+    }
 }
