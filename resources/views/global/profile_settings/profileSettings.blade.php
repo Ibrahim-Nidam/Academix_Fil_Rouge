05d322f9fea43183d9187eb892688dcc1297edf7
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-26 14:27:29 +0000

    VIEW: global profile page updated with welcoming header based on day time

--- resources/views/global/profile_settings/profileSettings.blade.php
+++ resources/views/global/profile_settings/profileSettings.blade.php
@@ -8,7 +8,7 @@
 {{-- header page --}}
     <div class="flex-1 p-5 px-16">
         <div class="mb-10 flex items-center gap-6 flex-wrap">
-            <h1 class="text-4xl font-bold text-primary-blue dark:text-primary-text-dark">Good Morning</h1>
+            <h1 class="text-4xl font-bold text-primary-blue dark:text-primary-text-dark">Good {{now()->hour < 12 ? 'Morning' : 'Evening'}}</h1>
             <h2 class="text-2xl text-gray-500 dark:text-gray-400 mt-1">Mr. Johnson Maduka</h2>
         </div>
         {{-- security & info buttons --}}
