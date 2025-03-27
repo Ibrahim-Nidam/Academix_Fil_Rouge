86f3657718706dfbe51bb75e1d00327f4e6efada
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-27 13:25:39 +0000

    VIEW: profile security updated with dynamic header based on user and time

--- resources/views/global/profile_settings/security.blade.php
+++ resources/views/global/profile_settings/security.blade.php
@@ -8,8 +8,8 @@
 {{-- header page --}}
     <div class="flex-1 p-5 px-16">
         <div class="mb-10 flex items-center gap-6 flex-wrap">
-            <h1 class="text-4xl font-bold text-primary-blue dark:text-primary-text-dark">Good Morning</h1>
-            <h2 class="text-2xl text-gray-500 dark:text-gray-400 mt-1">Mr. Johnson Maduka</h2>
+            <h1 class="text-4xl font-bold text-primary-blue dark:text-primary-text-dark">Good {{now()->hour < 12 ? 'Morning' : 'Evening'}}</h1>
+            <h2 class="text-2xl text-gray-500 dark:text-gray-400 mt-1">{{Auth::user()->gender == 'Male' ? 'Mr. ' : 'Ms. '}} {{ucfirst(Auth::user()->first_name) . ' ' . ucfirst(Auth::user()->last_name)}} </h2>
         </div>
         
         <div class="md:ml-20">
