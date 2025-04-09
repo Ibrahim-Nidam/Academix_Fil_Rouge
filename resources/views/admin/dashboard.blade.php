d2e722ae26ed9030e99ea2b8ea00a147386a7221
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-09 16:10:04 +0100

    VIEW: Admin Dashboard updated with users welcome name

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,7 @@
 
 {{-- WELCOME SECTION --}}
     <div class="bg-white dark:bg-gray-800 rounded-xl shadow-md p-4 md:p-6 mb-4 md:mb-6">
-      <h1 class="text-2xl md:text-3xl font-bold">Welcome back, <span class="text-primary-accent">Dr. Smith</span></h1>
+      <h1 class="text-2xl md:text-3xl font-bold">Welcome back, <span class="text-primary-accent">{{Auth::user()->gender == 'Male' ? 'Mr. ' : 'Ms. '}} {{ucfirst(Auth::user()->first_name) . ' ' . ucfirst(Auth::user()->last_name)}} </span></h1>
       <p class="text-gray-600 dark:text-gray-400 mt-2 text-sm md:text-base">
         <span id="current-date">Monday, March 6, 2025</span> • 
         <span class="italic">Here's an overview of today's progress</span>
