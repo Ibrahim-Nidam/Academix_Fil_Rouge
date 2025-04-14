6a0e676fbfe95dab196df68b65d208409af71e1c
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-14 10:28:53 +0100

    MODEL: classroom updated with eloquent relations

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -9,5 +9,20 @@ class Classroom extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name'];
 
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function teachers()
+    {
+        return $this->belongsToMany(User::class, 'classroom_teacher', 'classroom_id', 'teacher_id');
+    }
+
+    public function schedules()
+    {
+        return $this->hasMany(Schedule::class);
+    }
 }
