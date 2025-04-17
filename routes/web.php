6dee269909ff342295c071298366d8cb9b2c5c09
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-17 16:38:11 +0100

    ROUTE: to update a schedule added

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::prefix('Admin')->middleware(['auth', 'role:Admin'])->group(function () {
     Route::get('/planning/events', [ScheduleController::class, 'events']);
     Route::get('/planning', [ScheduleController::class, 'index'])->name('admin.planningPage');
     Route::post('/planning', [ScheduleController::class, 'store'])->name('admin.planningPage.store');
+    Route::put('/planning/{id}', [ScheduleController::class, 'update']);
 
     Route::get('/users', [UserController::class, 'index'])->name('admin.usersPage');
     Route::post('/users', [UserController::class, 'store'])->name('user.store');
