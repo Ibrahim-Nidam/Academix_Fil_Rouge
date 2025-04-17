09cad61ef0910807c85cfc7c0b344099835a67fe
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-17 15:58:13 +0100

    MODEL: schedule updated with fillables and relations

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
+    use HasFactory;
+    
+    protected $fillable = [
+        'classroom_id',
+        'title',
+        'teacher_id',
+        'day_of_week',
+        'start_time',
+        'end_time',
+        'room'
+    ];
 
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
 }
