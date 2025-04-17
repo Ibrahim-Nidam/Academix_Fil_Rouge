f2d41e21e5ba702f192120823ca22ba9e416f09d
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-17 19:05:20 +0100

    CONTROLLER: dashboard updated with view required data for doghnut charts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,5 +8,19 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    
+    public function index()
+    {
+        $maleStudents = User::where('role', 'Student')->where('gender', 'Male')->count();
+        $femaleStudents = User::where('role', 'Student')->where('gender', 'Female')->count();
+        $totalStudents = $maleStudents + $femaleStudents;
+
+        $maleStaff = User::where('role', 'Teacher')->where('gender', 'Male')->count();
+        $femaleStaff = User::where('role', 'Teacher')->where('gender', 'Female')->count();
+        $totalStaff = $maleStaff + $femaleStaff;
+
+        return view('admin.dashboard', compact(
+            'maleStudents', 'femaleStudents', 'totalStudents',
+            'maleStaff', 'femaleStaff', 'totalStaff'
+        ));
+    }
 }
