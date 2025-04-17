bd0288b61883a5e31c6a66845321eea8c95e2259
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-17 15:54:05 +0100

    MIGRATE: schedule table created and updated with columns used

---
+++ database/migrations/2025_04_10_334857_create_schedules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('classroom_id')->constrained('classrooms')->onDelete('cascade');
+            $table->uuid('teacher_id');
+            $table->foreign('teacher_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('day_of_week', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('room')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
