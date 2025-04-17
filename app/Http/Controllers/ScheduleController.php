5833cd0c063c98c51ac62eeadb2e049d47705173
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-17 16:17:31 +0100

    CONTROLLER: schedule updated with index method

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Schedule;
+use App\Models\User;
+use App\Models\Classroom;
 
 class ScheduleController extends Controller
 {
-    
+    public function index()
+    {
+        return view('admin.planningPage', [
+            'teachers' => User::where('role', 'Teacher')->get(),
+            'classes'  => Classroom::all(),
+            'schedules'=> Schedule::with(['teacher', 'classroom'])->get()
+        ]);
+    }
+
 }
