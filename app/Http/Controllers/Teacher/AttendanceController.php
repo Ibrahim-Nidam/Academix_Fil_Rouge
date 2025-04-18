6d92f8dc297c88548244e80f8837a842dcac3cda
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-18 17:01:12 +0100

    CONTROLLER: attendance updated with get classes for the day method

--- app/Http/Controllers/Teacher/AttendanceController.php
+++ app/Http/Controllers/Teacher/AttendanceController.php
@@ -6,5 +6,16 @@ namespace App\Http\Controllers\Teacher;
 
 class AttendanceController extends Controller
 {
+    public function getClassesForDay($day)
+    {
+        $teacherId = Auth::user()->id;
+
+        $classes = Schedule::with('classroom')
+                    ->where('teacher_id', $teacherId)
+                    ->where('day_of_week', $day)
+                    ->get();
+
+        return response()->json($classes);
+    }
 
 }
