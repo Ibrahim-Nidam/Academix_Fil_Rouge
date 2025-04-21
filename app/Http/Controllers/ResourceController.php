878ce0d969dd9389e1c591b6f83c1fc9eb2cd488
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-21 16:08:03 +0100

    CONTROLLER: resource created

---
+++ app/Http/Controllers/ResourceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resource;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class ResourceController extends Controller
+{
+
+}
