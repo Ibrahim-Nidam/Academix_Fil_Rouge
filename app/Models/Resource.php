1e543b63a5404a30f7663dccc7cd66c88df9b5ca
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-21 16:02:29 +0100

    MODEL: resource updated with fillables and relations

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
+    use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'teacher_id',
+        'file_path',
+        'file_name',
+        'file_type',
+        'file_size',
+        'description',
+        'downloads'
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
+
+    public function classrooms()
+    {
+        return $this->belongsToMany(Classroom::class, 'classroom_resource');
+    }
 }
