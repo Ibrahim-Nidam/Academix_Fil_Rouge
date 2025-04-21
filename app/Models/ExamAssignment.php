267703c8a22dc39253b226b4fc4631c0bb4c2747
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-21 06:01:58 +0100

    MODEL: exam assignment updated with relations and fillables

--- app/Models/ExamAssignment.php
+++ app/Models/ExamAssignment.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExamAssignment extends Model
 {
+    use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'classroom_id',
+        'teacher_id',
+        'type',
+        'date'
+    ];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function grades()
+    {
+        return $this->hasMany(Grade::class);
+    }
 }
