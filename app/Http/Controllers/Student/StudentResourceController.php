4490f3cc9870f7bf50e59669b918899d0f7edbc7
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-22 12:32:35 +0100

    CONTROLLER: student resource updated with index method

--- app/Http/Controllers/Student/StudentResourceController.php
+++ app/Http/Controllers/Student/StudentResourceController.php
@@ -7,5 +7,34 @@ use Illuminate\Http\Request;
 
 class StudentResourceController extends Controller
 {
-    //
-}
+    public function index()
+    {
+        $teachers = User::where('role', 'Teacher')
+            ->whereHas('resources')
+            ->with(['resources' => function($query) {
+                $query->with('tags');
+                $query->orderBy('created_at', 'desc');
+            }, 'subjects'])
+            ->get();
+
+        // group resources by grade level for each teacher
+        foreach ($teachers as $teacher) {
+            $groupedResources = [];
+            
+            foreach ($teacher->resources as $resource) {
+                $grade = 'General';
+            
+                if (! isset($groupedResources[$grade])) {
+                    $groupedResources[$grade] = [];
+                }
+            
+                $groupedResources[$grade][] = $resource;
+            }
+            
+            // Sort groups by grade level
+            ksort($groupedResources);
+            $teacher->groupedResources = $groupedResources;
+        }
+
+        return view('student.resources', compact('teachers'));
+    }
