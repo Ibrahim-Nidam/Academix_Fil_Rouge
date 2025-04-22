48240558b1ed85e417121540d49f5c682747ab30
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-22 10:19:17 +0100

    JAVASCRIPT: dash script updated with time logic

---
+++ resources/views/partials/student/js/dashScript.blade.php
@@ -0,0 +1,5 @@
+<script>
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById('current-date').textContent = now.toLocaleDateString('en-US', options);
+</script>
